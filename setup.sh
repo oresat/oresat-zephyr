3dee4cadd22f68f98acf8e3ad8048908af552df1
Chad Coates <chroco@gmail.com>
2024-09-21 12:43:16 -0600

    fix setup script and app_protocard build

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 ZEPHYR_VENV=".venv"
+WEST_WORKSPACE="workspace"
 
 if [ ! -d ${ZEPHYR_VENV} ]; then
   python3 -m venv ${ZEPHYR_VENV}
@@ -9,9 +10,13 @@ fi
 source ./${ZEPHYR_VENV}/bin/activate
 
 pip install west
-west init
+west init -m https://github.com/zephyrproject-rtos/zephyr --mr v3.7.0 ${WEST_WORKSPACE}
+cd ${WEST_WORKSPACE}
 west update
 west zephyr-export
 pip install -r ./zephyr/scripts/requirements.txt
 
-echo "run \"python3 -m venv ${ZEPHYR_VENV}\" to start virtual environment"
+export ZEPHYR_BASE=$(pwd)/zephyr
+echo "export ZEPHYR_BASE=$(pwd)/zephyr" >> ~/.bashrc
+echo "ZEPHYR_BASE=${ZEPHYR_BASE} appended to ~/.bashrc"
+echo "run source .venv/bin/activate to start virtual environment"
