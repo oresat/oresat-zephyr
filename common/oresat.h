c5ceec3a06cfb30d92f4f8f5c0a2297c9d2a81e5
Ryan Medick <rmedick@protonmail.com>
2024-10-11 16:42:41 -0700

    fix issue with reading option bytes for node id

--- common/oresat.h
+++ common/oresat.h
@@ -1,14 +1,20 @@
 #ifndef ORESAT_H
 #define ORESAT_H
 
+#ifdef STM32F091xC
 #include "stm32f091xc.h"
+#endif
 
 static inline uint8_t get_node_id(void) {
-	uint8_t node_id = READ_BIT(FLASH->OBR, FLASH_OBR_DATA0);
+#ifdef STM32F091xC
+	uint8_t node_id = (FLASH->OBR & FLASH_OBR_DATA0_Msk) >> FLASH_OBR_DATA0_Pos;
 	if (node_id == 0 || node_id > 0x7C)  {
 		node_id = 0x7C;
 	}
   return node_id;
+#else
+  return 0x7c;
+#endif
 }
 
 #endif
