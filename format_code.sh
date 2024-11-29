d6ff5781738040da5086937af7c6da956f39c145
Ryan Medick <rmedick@protonmail.com>
2024-11-29 10:24:23 -0800

    fix formt_code.sh script

--- format_code.sh
+++ format_code.sh
@@ -3,4 +3,4 @@
 # a .clang-format-ignore file will only be used by clang-format v18.0.1 or newer.
 #
 # This method supports older clang-format versions.
-find . -wholename "*.[h/c]" -not -path "./common/canopennode_v4/CANopenNode/*" | xargs clang-format -i --verbose
+find . -wholename "*.[h/c]" -not -path "*/gen/*" -not -path "*/build/*" -not -path "*/CANopenNode/*" | xargs clang-format -i $@
